429efa805863bfb32a9c0d9ae83a63d5b0f748db
ieadeleke <eadelekeife@yahoo.com>
2025-09-21 16:29:51 +0100

    app complete setup

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,5 +1,6 @@
 // const API_BASE = process.env.NEXT_PUBLIC_API_BASE || "http://localhost:8800/api/v1";
-const API_BASE = process.env.NEXT_PUBLIC_API_BASE || "https://cfdrockets-be.onrender.com/api/v1";
+// const API_BASE = process.env.NEXT_PUBLIC_API_BASE || "https://cfdrockets-be.onrender.com/api/v1";
+const API_BASE = process.env.NEXT_PUBLIC_API_BASE || "https://tradar-be.onrender.com/api/v1";
 
 
 function token() {
